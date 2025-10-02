8bf85794c021730eabde55cd29248bf5dcbd0008
Kaif Ayaz Khan <kaifayazkhan@gmail.com>
2025-10-02 14:35:03 +0530

    feat: enhance db logging and configure logger for production

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,24 +1,34 @@
 import pino from 'pino';
 import { env } from './env.js';
 
-const transport = pino.transport({
-  targets: [
-    {
-      target: '@logtail/pino',
-      options: {
-        sourceToken: env.BETTER_STACK_SOURCE_TOKEN,
-        options: { endpoint: env.BETTER_STACK_ENDPOINT },
+let transport;
+
+if (env.NODE_ENV === 'production') {
+  transport = pino.transport({
+    targets: [
+      {
+        target: '@logtail/pino',
+        options: {
+          sourceToken: env.BETTER_STACK_SOURCE_TOKEN,
+          options: { endpoint: env.BETTER_STACK_ENDPOINT },
+        },
       },
+    ],
+  });
+} else {
+  transport = pino.transport({
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
     },
-    {
-      target: 'pino-pretty',
-    },
-  ],
-});
+  });
+}
 
 const logger = pino(
   {
-    level: 'info',
+    level: env.NODE_ENV === 'development' ? 'debug' : 'info',
   },
   transport
 );
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,21 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { env } from '../config/env.js';
 import * as schema from './schema.js';
+import logger from '../config/logger.js';
 
-const db = drizzle(env.DATABASE_URL, { logger: true, schema: schema });
+const db = drizzle(env.DATABASE_URL, {
+  schema,
+  logger: {
+    logQuery(query, params) {
+      logger.debug(
+        {
+          query,
+          params: env.NODE_ENV === 'production' ? '[REDACTED]' : params,
+        },
+        'DB query'
+      );
+    },
+  },
+});
 
 export { db };
